80eb8001d79242815e8dbf7779b6af13689f6e57
Rachel S. Lee <rslee0025@gmail.com>
2020-06-10 19:10:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,28 @@
-class Artist
+class Artist 
 
-	attr_accessor :name
-	attr_reader :songs
+  attr_accessor :name
+  @@all = []
 
-	@@song_count = 0
-
-	def initialize(name)
-		@name = name
-		@songs = []
-	end
-
-  def add_song(song)
-  	song.artist = self
-  	@songs << song
-  	@@song_count += 1
+  def initialize(name)
+    @name = name
+    @@all << self
   end
 
-  def add_song_by_name(title)
-  	song = Song.new(title)
-    add_song(song)
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 
-  def self.song_count
-  	@@song_count
+  def genres
+    songs.map {|song| song.genre}
   end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(name, self, genre)
+  end
+
 
 end 
