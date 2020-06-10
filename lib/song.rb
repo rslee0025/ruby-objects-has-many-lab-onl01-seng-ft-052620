a8cbd7ddcfbc2298cc67d68dd82fbb6a8890332d
Rachel S. Lee <rslee0025@gmail.com>
2020-06-10 18:38:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,18 @@
-class Song 
-  attr_accessor :artist, :name
-  
-  @@all = []
-  
-  def initialize
-    @name = name 
-  end 
-  
-  def self.all 
-    @@all
+class Song
+
+	attr_accessor :artist
+	attr_reader :name
+
+  def initialize(name)
+  	@name = name
   end
-  
-  def save 
-    @@all << self
-  end 
-  
-  
+
+  def artist_name
+  	if self.artist
+  		self.artist.name
+  	else
+  		nil
+  	end
+  end
+
 end 
