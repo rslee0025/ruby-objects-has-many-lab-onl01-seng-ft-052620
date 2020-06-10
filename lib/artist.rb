ce275f6b17621018bfcaf682957b8e223d85b586
Rachel S. Lee <rslee0025@gmail.com>
2020-06-10 19:47:48 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+
+	attr_accessor :name
+	attr_reader :songs
+
+	@@song_count = 1
+
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+	
+	 def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+
+  def add_song(song)
+  	song.artist = self
+  	@songs << song
+  	@@song_count += 1
+  end
+
+  def add_song_by_name(title)
+  	song = Song.new(title)
+    add_song(song)
+  end
+
+  def self.song_count
+  	@@song_count
+  end
+
+end 
+
+ 
+ 
---
+++ lib/author.rb
@@ -0,0 +1,36 @@
+class Author
+
+	attr_accessor :name
+	attr_reader :posts
+
+	@@post_count = 1
+
+	def initialize(name)
+		@name = name
+		@posts = []
+	end
+	
+	 def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+
+  def add_post(post)
+  	post.author = self
+  	@posts << post
+  	@@post_count += 1
+  end
+
+  def add_post_by_title(title)
+  	post = Post.new(title)
+    add_post(post)
+  end
+
+  def self.post_count
+  	@@post_count
+  end
+
+end 
+
+ 
+ 
