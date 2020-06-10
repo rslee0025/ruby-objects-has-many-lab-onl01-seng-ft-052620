078125cd87ec16ec70cd8ba5b6a8f5e77f045c25
Rachel S. Lee <rslee0025@gmail.com>
2020-06-10 19:09:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,28 @@
-require 'pry'
 class Artist
-  attr_accessor :name
 
-  def initialize(name)
-    @name = name
-    @songs = []
-  end
+	attr_accessor :name
+	attr_reader :songs
+
+	@@song_count = 0
+
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
 
   def add_song(song)
-    @songs << song
-    song.artist = self
+  	song.artist = self
+  	@songs << song
+  	@@song_count += 1
   end
 
-  def songs
-    @songs
+  def add_song_by_name(title)
+  	song = Song.new(title)
+    add_song(song)
   end
 
- 
-end
+  def self.song_count
+  	@@song_count
+  end
+
+end 
