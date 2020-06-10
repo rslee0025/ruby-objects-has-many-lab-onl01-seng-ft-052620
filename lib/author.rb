4c9ef1c674b8e100bda75d91582d9ad277e93710
Rachel S. Lee <rslee0025@gmail.com>
2020-06-10 19:31:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,28 +1,28 @@
-class Author 
-  attr_accessor :name 
-  
-  	@@post_count = 0
-  
-  def initialize(name)
-    @name = name 
-  end
-  
-  def posts
-    Post.all.select {|post| post.author == self}
-  end
-  
+class Author
+
+	attr_accessor :name
+	attr_reader :posts
+
+	@@post_count = 0
+
+	def initialize(name)
+		@name = name
+		@posts = []
+	end
+
   def add_post(post)
   	post.author = self
   	@posts << post
   	@@post_count += 1
   end
-  
+
   def add_post_by_title(title)
   	post = Post.new(title)
   	add_post(post)
   end
-  
-   def self.post_count
+
+  def self.post_count
   	@@post_count
   end
+
 end 
